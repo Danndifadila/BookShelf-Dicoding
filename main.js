a1d9ce0a48141ef617f2ce9d005219bcdacf8d5e
Danndifadila <danndifadilla@gmail.com>
2025-02-09 12:04:35 +0700

    Adding books to an object

---
+++ main.js
@@ -0,0 +1,24 @@
+// Declerate an object 
+const books = [];
+
+// Make button not reload the page
+
+// Add book to the list
+
+// Generate unique id for each book
+
+// Adding books to an object
+
+// Make container in html for books
+
+// Add finish reading, delete books, and edit books
+
+// Make finish button
+
+// Make delete book button
+
+// Make finish button function
+
+// Make delete button function
+
+// All button activities
